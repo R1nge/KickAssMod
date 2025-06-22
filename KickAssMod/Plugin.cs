db32a88f97614887cd1bc84fba5df499c321a378
R1nge <r1ngegamedev@gmail.com>
2025-06-22 16:48:30 +0300

    its doing smth

--- KickAssMod/Plugin.cs
+++ KickAssMod/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace KickAssMod;
 
@@ -13,5 +14,30 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(typeof(Patch01));
     }
+}
+
+[HarmonyPatch(typeof(AirportCheckInKiosk))]
+[HarmonyPatch("IsInteractable")] // if possible use nameof() here
+class Patch01
+{
+    static bool Prefix(AirportCheckInKiosk __instance, ref Character ___character)
+    {
+        return false;
+    }
+}
+
+public class AirportCheckInKiosk
+{
+    // Token: 0x0600044B RID: 1099 RVA: 0x00019722 File Offset: 0x00017922
+    public bool IsInteractible(Character interactor)
+    {
+        return true;
+    }
+}
+
+public class Character
+{
+    
 }
